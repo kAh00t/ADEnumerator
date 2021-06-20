440b6976961575b5116286c197bfc6dc73824c0d
kAh00t <david.mediology@gmail.com>
2021-06-20 19:26:37 +0100

    Update ADEnumerator-LocalOnly.ps1

--- ADEnumerator-LocalOnly.ps1
+++ ADEnumerator-LocalOnly.ps1
@@ -1,24 +1,6 @@
-# get-psdrive ENV:
-# get-childitem ENV:
-param(
-
-     [Parameter()]
-     [string[]]$inputList,
- 
-     [Parameter()]
-     [string]$credentials
-)
-
-
-
 function EnumerateEachMachine
 {
-
-# $computerList = Get-Content $inputList
-
-# foreach ($computername in $computerList)
-# {
-        
+    
         function Get-AntiVirusProduct {
         [CmdletBinding()]
         param (
@@ -263,90 +245,71 @@ Get-CimInstance
                     
                 }
                 
-                
-   
-                
         ######################## Variables ##############################
 
                 
                 # to add: Windows Firewall Enabled, DomainName, Windows Automatic updates set NBT over TCP/IP disabled on all interfaces, account lockout, local password policy , Test-ComputerSecureChannel?, 
                 # to double check: the IP address is currently looking for the first IPv4 match, will that work if they are on a VPN/different adapater? mmm
                 $PassPol = Get-PassPol
+                Write-Host "Password-Policy:$PassPol"
                 $ipAddress = $(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | out-null; $Matches[1])
+                Write-Host "IP:$ipAddress"
                 $computerName = $env:COMPUTERNAME
+                Write-Host "ComputerName:$computerName"
                 $firewallStatusDomain = (Get-NetFirewallProfile | select Name,Enabled | where Name -in "Domain" ).enabled 
+                Write-Host "FirewallStatusDomain:$firewallStatusDomain"
                 $firewallStatusPrivate = (Get-NetFirewallProfile | select Name,Enabled | where Name -in "Private" ).enabled
+                Write-Host "FirewallStatusPrivate:$firewallStatusPrivate"
                 $firewallStatusPublic = (Get-NetFirewallProfile | select Name,Enabled | where Name -in "Public").enabled
+                Write-Host "FirewallStatusPublic:$firewallStatusPublic"
                 $smbV1Enable = (Get-SmbServerConfiguration | select EnableSMB1Protocol).EnableSMB1Protocol
+                Write-Host "SMBv1:$smbV1Enable"
                 $smbEncryptionEnabled = (Get-SmbServerConfiguration | select EncryptData).EncryptData
+                Write-Host "SMBEncryptionEnabled:$smbEncryptionEnabled"
                 $smbSigningEnabled = (Get-SmbServerConfiguration | select RequireSecuritySignature).requiresecuritysignature
-                $llmnrstatus = (Get-ItemProperty -path 'HKLM:\Software\policies\Microsoft\Windows NT\DNSClient').EnableMulticast
-               
+                Write-Host "SMBSigningEnabled:$smbSigningEnabled"
+                $llmnrstatus = (Get-ItemProperty -path 'HKLM:\Software\policies\Microsoft\Windows NT\DNSClient' -ErrorAction SilentlyContinue).EnableMulticast
+                Write-Host "LLMNRStatus:$llmnrstatus"
                 $ipv6Enabled = Get-NetIPInterface | where AddressFamily -in "IPv6" | select DHCP | where DHCP -Contains Enabled; If ($ipv6Enabled -ne $empty) {$ipv6EnabledStatus = 1} Else {$ipv6EnabledStatus = 0};
+                Write-Host "IPv6Enabled:$ipv6Enabled"
                 $ldapSigningEnabled = (Get-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Services\LDAP').LdapclientIntegrity 
+                Write-Host "LDAPSigningEnabled:$ldapSigningEnabled"
                 $lastSecurityUpdate = (Get-HotFix -Description Security* | Sort-Object -Property InstalledOn)[-1].installedon
+                Write-Host "LastSecurityUpdate:$lastSecurityUpdate"
                 $localAdminAccountEnabled = (Get-LocalUser | select Name,Enabled | where Name -in "Administrator").Enabled
+                Write-Host "LocalAdminAccountEnabled:$localAdminAccountEnabled"
                 $localGuestAccountEnabled = (Get-LocalUser | select Name,Enabled | where Name -in "Guest").enabled
+                Write-Host "LocalGuestAccountEnabled:$localGuestAccountEnabled"
                 $defaultGateway = (Get-NetIPConfiguration | Foreach IPv4DefaultGateway).nexthop
+                Write-Host "DefaultGateway:$defaultGateway"
                 $windowsVersion = ([environment]::OSVersion.Version).build
+                Write-Host "WindowsVersion:$windowsVersion"
                 $windowsVersionMajor = ([environment]::OSVersion.Version).Major
+                Write-Host "WindowsVersionMajor:$windowsVersionMajor"
                 $windowsEdition = (Get-WmiObject Win32_OperatingSystem).Caption
+                Write-Host "WindowsEdition:$windowsEdition"
                 $unsupportedOS = 10240,10586,14393,15063,16299,17134,17763,18362,18363
                 $osSupported = $OSVersion -notin $unsupportedOS
+                Write-Host "OSSupported:$osSupported"
                 $avName = (Get-AntiVirusProduct).'Name'        
+                Write-Host "AVName:$avName"
                 $avRealTimeProtectStatus = (Get-AntiVirusProduct).'Real-Time Protection Status'
+                Write-Host "AVRealTimeProtectStatus:$avRealTimeProtectStatus"
                 $avDefinitions = (Get-AntiVirusProduct).'Definition Status' 
             
-
+                Write-Host "AVDefinition:$avDefinitions"
                 $minPassLength = $PassPol.'Minimum Password Length (Chars)'
+                Write-Host "MinPassLength:$minPassLength"
                 $passHistoryVar = $PassPol.'Enforce Password History (Passwords remembered)'
+                Write-Host "MinPassHistory:$passHistoryVar"
                 $acctLockoutThresholdVar = $PassPol.'Account Lockout Threshold (Invalid logon attempts)'
+                Write-Host "AcctLockoutThreshold:$acctLockoutThresholdVar"
                 $acctLockoutDurationVar = $PassPol.'Account Lockout Duration (Minutes)'
-                
-                
-                $obj = new-object psobject -Property @{
-                            IPAddress = $ipAddress
-                            ComputerName = $computerName
-                            OSVersion = $windowsVersion
-                            OSEdition = $windowsEdition
-                            OSMajor = $windowsVersionMajor
-                            OSSupported = $osSupported
-                            FWDomainEnabled = $firewallStatusDomain
-                            FWPrivateEnabled = $firewallStatusPrivate
-                            FWPublicEnabled = $firewallStatusPublic
-                            SMBv1Enabled = $smbV1Enable
-                            SMBEncryption = $smbEncryptionEnabled 
-                            SMBSigning = $smbSigningEnabled 
-                            LLMNREnabled = $llmnrstatus
-                            IPv6Enabled = $ipv6EnabledStatus
-                            LDAPSigning = $ldapSigningEnabled
-                            LastSecurityUpdate = $lastSecurityUpdate
-                            DefaultAdminEnabled = $localAdminAccountEnabled
-                            DefaultGuestEnabled = $localGuestAccountEnabled
-                            DefaultGateway = $defaultGateway
-                            AVName = $avName
-                            AVRealTimeProtectionStatus = $avRealTimeProtectStatus
-                            AVDefinitions = $avDefinitions
-                        
-                      
-                            MinPassLength = $minPassLength
-                            PasswordReuseDisabled = $passHistoryVar
-                            AcctLockoutAfter = $acctLockoutThresholdVar
-                            AcctDuration = $acctLockoutDurationVar
-                            
-                    }
-                    
-                #$obj | Format-Table ComputerName,IPAddress,DefaultGateway,OSEdition,OSVersion,OSSupported,FWDomainEnabled,FWPrivateEnabled,FWPublicEnabled,SMBv1Enabled,SMBEncryption,SMBSigning,LLMNREnabled,LDAPSigning,IPv6Enabled,LastSecurityUpdate,DefaultAdminEnabled,DefaultGuestEnabled,AVName,AVRealTimeProtectionStatus,AVDefinitions,MinPassLength,PasswordReuseDisabled,AcctLockoutAfter,AcctDuration
-                
-                $obj | Format-Table ComputerName,IPAddress,DefaultGateway,OSEdition,OSVersion,OSSupported,FWDomainEnabled,FWPrivateEnabled,FWPublicEnabled,SMBv1Enabled,SMBEncryption,SMBSigning,LLMNREnabled,LDAPSigning,IPv6Enabled,LastSecurityUpdate,DefaultAdminEnabled,DefaultGuestEnabled,AVName,AVRealTimeProtectionStatus,AVDefinitions,MinPassLength,PasswordReuseDisabled,AcctLockoutAfter,AcctDuration
-                #$var = $minPassLength
-                #write-host $OtherVar
-              
+                Write-Host "AccountLockoutDuration:$acctLockoutDurationVar"
                 
                  
 # }
 
 }
-
 EnumerateEachMachine
 
